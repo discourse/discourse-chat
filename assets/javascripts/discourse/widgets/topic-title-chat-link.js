9db2be97a409d1934dc8f93f81e3c8d912e9c0e3
Mark VanLandingham <markvanlan@gmail.com>
2021-07-16 10:33:08 -0500

    cleanup

--- assets/javascripts/discourse/widgets/topic-title-chat-link.js
+++ assets/javascripts/discourse/widgets/topic-title-chat-link.js
@@ -8,15 +8,12 @@ export default createWidget("topic-title-chat-link", {
   title: "chat.open",
 
   html(topic) {
-    if (!topic.has_chat_live) {
-      return;
+    if (topic.has_chat_live) {
+      return iconNode("far-comments");
     }
-
-    return iconNode("far-comments");
   },
 
   click() {
-    console.log(this.attrs);
     this.appEvents.trigger("chat:open-channel", this.attrs);
   },
 });

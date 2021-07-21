3f188ed51ffce0fb4d7ab9da7701b8344bf7cebc
Mark VanLandingham <mark.vanlandingham@discourse.org>
2021-07-21 09:33:09 -0500

    UX: Remove collapse functionality in channel selector (#34)

--- assets/javascripts/discourse/components/chat-channel-row.js
+++ assets/javascripts/discourse/components/chat-channel-row.js
@@ -1,19 +1,12 @@
 import Component from "@ember/component";
-import { action } from "@ember/object";
 import discourseComputed from "discourse-common/utils/decorators";
 
 export default Component.extend({
   channel: null,
   switchChannel: null,
-  expanded: true,
   nested: false,
 
   click() {
     this.switchChannel(this.channel);
-  },
-
-  @action
-  toggleExpand() {
-    this.set("expanded", !this.expanded);
   },
 });

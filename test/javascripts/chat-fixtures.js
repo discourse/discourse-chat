c9d787997fc6443a45eca4436bfa49044f74fee1
Mark VanLandingham <markvanlan@gmail.com>
2021-08-13 13:39:01 -0500

    prettier

--- test/javascripts/chat-fixtures.js
+++ test/javascripts/chat-fixtures.js
@@ -29,41 +29,41 @@ export const chatChannels = {
       },
       chat_channels: [
         {
-        id: 4,
-        chatable_id: 12,
-        chatable_type: "Topic",
-        chatable_url: "http://localhost:3000/t/small-action-testing-topic/12",
-        title: "Small action - testing topic",
-        chatable: {
-          id: 12,
+          id: 4,
+          chatable_id: 12,
+          chatable_type: "Topic",
+          chatable_url: "http://localhost:3000/t/small-action-testing-topic/12",
           title: "Small action - testing topic",
-          fancy_title: "Small action - testing topic",
-          slug: "small-action-testing-topic",
-          posts_count: 1,
+          chatable: {
+            id: 12,
+            title: "Small action - testing topic",
+            fancy_title: "Small action - testing topic",
+            slug: "small-action-testing-topic",
+            posts_count: 1,
+          },
+          chat_channels: [],
         },
-        chat_channels: [],
-      },
-      {
-        id: 11,
-        chatable_id: 80,
-        chatable_type: "Topic",
-        chatable_url:
-          "http://localhost:3000/t/coolest-thing-you-have-seen-today/80",
-        title: "Coolest thing you have seen today",
-        chatable: {
-          id: 80,
+        {
+          id: 11,
+          chatable_id: 80,
+          chatable_type: "Topic",
+          chatable_url:
+            "http://localhost:3000/t/coolest-thing-you-have-seen-today/80",
           title: "Coolest thing you have seen today",
-          fancy_title: "Coolest thing you have seen today",
-          slug: "coolest-thing-you-have-seen-today",
-          posts_count: 100,
+          chatable: {
+            id: 80,
+            title: "Coolest thing you have seen today",
+            fancy_title: "Coolest thing you have seen today",
+            slug: "coolest-thing-you-have-seen-today",
+            posts_count: 100,
+          },
+          chat_channels: [],
         },
-        chat_channels: [],
-      },
       ],
     },
   ],
-  direct_message_channels: []
-}
+  direct_message_channels: [],
+};
 
 export const chatView = {
   topic_chat_view: {
@@ -73,32 +73,32 @@ export const chatView = {
     can_delete_others: false,
     messages: [
       {
-      id: 174,
-      message: messageContents[0],
-      action_code: null,
-      created_at: "2021-07-20T08:14:16.950Z",
-      flag_count: 0,
-      user: {
-        id: 1,
-        username: "markvanlan",
-        name: null,
-        avatar_template: "/letter_avatar_proxy/v4/letter/m/48db29/{size}.png",
+        id: 174,
+        message: messageContents[0],
+        action_code: null,
+        created_at: "2021-07-20T08:14:16.950Z",
+        flag_count: 0,
+        user: {
+          id: 1,
+          username: "markvanlan",
+          name: null,
+          avatar_template: "/letter_avatar_proxy/v4/letter/m/48db29/{size}.png",
+        },
       },
-    },
-    {
-      id: 175,
-      message: messageContents[1],
-      action_code: null,
-      created_at: "2021-07-20T08:14:22.043Z",
-      in_reply_to_id: 174,
-      flag_count: 0,
-      user: {
-        id: 2,
-        username: "hawk",
-        name: null,
-        avatar_template: "/letter_avatar_proxy/v4/letter/m/48db29/{size}.png",
+      {
+        id: 175,
+        message: messageContents[1],
+        action_code: null,
+        created_at: "2021-07-20T08:14:22.043Z",
+        in_reply_to_id: 174,
+        flag_count: 0,
+        user: {
+          id: 2,
+          username: "hawk",
+          name: null,
+          avatar_template: "/letter_avatar_proxy/v4/letter/m/48db29/{size}.png",
+        },
       },
-    },
     ],
   },
 };

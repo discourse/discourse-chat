6d9976bce83128e1e093d0fb9bc1ef3dba6852fc
Mark VanLandingham <markvanlan@gmail.com>
2021-08-18 16:06:21 -0500

    logic

--- assets/javascripts/discourse/services/chat.js
+++ assets/javascripts/discourse/services/chat.js
@@ -147,7 +147,7 @@ export default Service.extend({
       this.currentUser.notifyPropertyChange("chat_channel_tracking_state");
 
       // Update updated_at timestamp for channel if direct message
-      const dmChatChannel = this.directMessageChannels.findBy(
+      const dmChatChannel = (this.directMessageChannels || []).findBy(
         "id",
         parseInt(channelId, 10)
       );

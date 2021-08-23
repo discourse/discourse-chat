b5d44a7879bc66376fff8ce61e2c84deba3fea69
Mark VanLandingham <markvanlan@gmail.com>
2021-08-23 13:24:55 -0500

    dont give up

--- app/models/chat_webhook_event.rb
+++ app/models/chat_webhook_event.rb
@@ -3,4 +3,7 @@
 class ChatWebhookEvent < ActiveRecord::Base
   belongs_to :chat_message
   belongs_to :incoming_chat_webhook
+
+  delegate :username, to: :incoming_chat_webhook
+  delegate :emoji, to: :incoming_chat_webhook
 end
--- app/serializers/chat_webhook_event_serializer.rb
+++ app/serializers/chat_webhook_event_serializer.rb
@@ -3,12 +3,4 @@
 class ChatWebhookEventSerializer < ApplicationSerializer
   attributes :username,
              :emoji
-
-  def username
-    object.incoming_chat_webhook.username
-  end
-
-  def emoji
-    object.incoming_chat_webhook.emoji
-  end
 end

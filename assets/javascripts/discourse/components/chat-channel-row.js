be9d0900014850c765f7b0fa27ff71ae823725a7
Jordan Vidrine <jordan@jordanvidrine.com>
2021-08-24 13:50:25 -0500

    Remove unneeded imports, computed properties

--- assets/javascripts/discourse/components/chat-channel-row.js
+++ assets/javascripts/discourse/components/chat-channel-row.js
@@ -11,10 +11,5 @@ export default Component.extend({
   click() {
     this.switchChannel(this.channel);
     return false; // Don't propogate click to potential parent channel
-  },
-
-  @discourseComputed("currentUser.chat_channel_tracking_state")
-  unreadCount(trackingState) {
-    return trackingState[this.channel.id]?.unread_count || 0;
-  },
+  }
 });

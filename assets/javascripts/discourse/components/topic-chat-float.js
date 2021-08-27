604db8b210863c712680916e762f0875664dada9
Mark VanLandingham <mark.vanlandingham@discourse.org>
2021-08-26 19:20:05 -0500

    FIX: Revert a couple commits (#122)
    * Revert "adjust padding on header (#120)"
    
    This reverts commit 90d64030a9089ab5836430660295258ebe76302c.
    
    * Revert "Sidebar design (#119)"
    
    This reverts commit 297c58aa19f2b36533d888da7039571cc71aedb7.

--- assets/javascripts/discourse/components/topic-chat-float.js
+++ assets/javascripts/discourse/components/topic-chat-float.js
@@ -139,7 +139,6 @@ export default Component.extend({
   _fireHiddenAppEvents() {
     this.chatService.setChatOpenStatus(!this.hidden);
     this.appEvents.trigger("chat:rerender-header");
-    this.appEvents.trigger("chat:float-closed");
   },
 
   _loadCookFunction() {
@@ -268,31 +267,33 @@ export default Component.extend({
   toggleExpand() {
     this.set("expanded", !this.expanded);
     if (this.expanded === false) {
-      document.body.classList.remove("chat-open");
-      document.body.classList.add("chat-minimized");
+      document.body.classList.remove("mobile-chat-open");
+      document.body.classList.add("mobile-chat-minimized");
     } else {
-      document.body.classList.add("chat-open");
-      document.body.classList.remove("chat-minimized");
+      document.body.classList.add("mobile-chat-open");
+      document.body.classList.remove("mobile-chat-minimized");
     }
   },
 
   @action
   close() {
     this.set("hidden", true);
-    document.body.classList.remove("chat-open");
-    document.body.classList.remove("chat-minimized");
+    document.body.classList.remove("mobile-chat-open");
+    document.body.classList.remove("mobile-chat-minimized");
   },
 
   @action
   toggleChat() {
     this.set("hidden", !this.hidden);
     if (this.hidden) {
-      document.body.classList.remove("chat-open");
       return;
     } else {
       this.set("expanded", true);
-      document.body.classList.remove("chat-minimized");
-      document.body.classList.add("chat-open");
+      let html = document.documentElement;
+      if (html.classList.contains("mobile-view")) {
+        document.body.classList.remove("mobile-chat-minimized");
+        document.body.classList.add("mobile-chat-open");
+      }
       if (this.activeChannel) {
         // Channel was previously open, so after expand we are done.
         return;

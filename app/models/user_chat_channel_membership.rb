e1ec784065217fa87f15c134eff6eef2d378cbae
Mark VanLandingham <markvanlan@gmail.com>
2021-09-14 13:22:41 -0500

    one more little thing

--- app/models/user_chat_channel_membership.rb
+++ app/models/user_chat_channel_membership.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class UserChatChannelMembership < ActiveRecord::Base
-  attribute :unread_count, default: 0
-
   belongs_to :user
   belongs_to :chat_channel
   belongs_to :last_read_message, class_name: "ChatMessage", optional: true

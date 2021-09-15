b30a9e30a45c922d591ffa37812c7b5780036133
Jordan Vidrine <jordan@jordanvidrine.com>
2021-09-15 16:33:15 -0500

    hehe

--- assets/javascripts/discourse/components/chat-channel-title.js
+++ assets/javascripts/discourse/components/chat-channel-title.js
@@ -3,7 +3,8 @@ import Component from "@ember/component";
 import { gt } from "@ember/object/computed";
 
 export default Component.extend({
-  tagName: "",
+  tagName: "span",
+  classNameBindings: [":chat-channel-title","unreadCount:has-unread"],
   channel: null,
   multiDm: gt("channel.chatable.users.length", 1),
 

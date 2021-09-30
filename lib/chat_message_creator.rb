ba7c99bea71f75c9055cfb9778160d159d686475
Mark VanLandingham <markvanlan@gmail.com>
2021-09-30 11:16:45 -0500

    woops

--- lib/chat_message_creator.rb
+++ lib/chat_message_creator.rb
@@ -35,14 +35,14 @@ class DiscourseChat::ChatMessageCreator
   end
 
   def create
-    # begin
+    begin
       @chat_message.save!
       mentioned_user_ids = create_mention_notifications
       notify_watching_users(except: [@user.id] + mentioned_user_ids)
       ChatPublisher.publish_new!(@chat_channel, @chat_message, @staged_id)
-    # rescue => error
-      # @error = error
-    # end
+    rescue => error
+      @error = error
+    end
   end
 
   def failed?

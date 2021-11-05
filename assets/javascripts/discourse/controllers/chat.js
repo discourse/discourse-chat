8f34b797380b3abecc853e9b62eb6cb245649ea7
Mark VanLandingham <markvanlan@gmail.com>
2021-11-05 09:31:02 -0500

    Finish UI?

--- assets/javascripts/discourse/controllers/chat.js
+++ assets/javascripts/discourse/controllers/chat.js
@@ -1,4 +1,16 @@
 import Controller from "@ember/controller";
+import showModal from "discourse/lib/show-modal";
+import { action } from "@ember/object";
 
 export default Controller.extend({
-})
+  @action
+  openFollowModal() {
+    showModal("chat-channel-settings");
+  },
+
+  @action
+  startCreatingDm(e) {
+    this.appEvents.trigger("chat:start-new-dm");
+    return false;
+  },
+});

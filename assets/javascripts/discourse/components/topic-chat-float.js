2b6b7b5d7a6ffb354391c857572381a5ab13603a
Mark VanLandingham <markvanlan@gmail.com>
2021-11-29 09:00:11 -0600

    FIX: Navigate to full-page in mobile view

--- assets/javascripts/discourse/components/topic-chat-float.js
+++ assets/javascripts/discourse/components/topic-chat-float.js
@@ -129,7 +129,16 @@ export default Component.extend({
       ajax(`/chat/chat_channels/${channel.id}/follow`, { method: "POST" })
         .then(() => {
           this.chat.startTrackingChannel(channel);
-          this.switchChannel(channel);
+
+          if (this.site.mobileView) {
+            return this.router.transitionTo(
+              "chat.channel",
+              channel.id,
+              channel.title
+            );
+          } else {
+            this.switchChannel(channel);
+          }
         })
         .catch(popupAjaxError);
     } else {

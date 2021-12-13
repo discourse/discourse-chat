129caf1f476f91fb080d3b6ecf3976c2ee1f2cc4
Mark VanLandingham <markvanlan@gmail.com>
2021-12-13 09:36:39 -0600

    FIX: Make sure emoji list exists before mapping

--- assets/javascripts/discourse/components/chat-composer.js
+++ assets/javascripts/discourse/components/chat-composer.js
@@ -411,11 +411,13 @@ export default Component.extend(TextareaTextManipulation, ComposerUploadUppy, {
 
           return resolve(options);
         })
-          .then((list) =>
-            list.map((code) => {
-              return { code, src: emojiUrlFor(code) };
-            })
-          )
+          .then((list) => {
+            if (list) {
+              list.map((code) => {
+                return { code, src: emojiUrlFor(code) };
+              });
+            }
+          })
           .then((list) => {
             if (list.length) {
               list.push({ label: I18n.t("composer.more_emoji"), term });

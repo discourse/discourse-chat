9b3f99c9feddfcb37f3b8eb4d967eeb195dd31a3
Mark VanLandingham <markvanlan@gmail.com>
2021-12-13 11:25:36 -0600

    lint

--- assets/javascripts/discourse/controllers/preferences-chat.js
+++ assets/javascripts/discourse/controllers/preferences-chat.js
@@ -1,7 +1,8 @@
-import { action } from "@ember/object";
-import { popupAjaxError } from "discourse/lib/ajax-error";
 import Controller from "@ember/controller";
 import discourseComputed from "discourse-common/utils/decorators";
+import I18n from "I18n";
+import { action } from "@ember/object";
+import { popupAjaxError } from "discourse/lib/ajax-error";
 import { CHAT_SOUNDS } from "discourse/plugins/discourse-chat/discourse/initializers/chat-notification-sounds";
 
 const chatAttrs = [
--- db/migrate/20211213150607_add_chat_sound_to_user_options.rb
+++ db/migrate/20211213150607_add_chat_sound_to_user_options.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class AddChatSoundToUserOptions < ActiveRecord::Migration[6.1]
   def change
     add_column :user_options, :chat_sound, :string, null: true

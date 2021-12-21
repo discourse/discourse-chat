3b3d80aa31a73becb7aff47ba0f70cd44b5616f5
Mark VanLandingham <markvanlan@gmail.com>
2021-12-21 15:42:48 -0600

    linting

--- assets/javascripts/discourse/controllers/create-channel-modal.js
+++ assets/javascripts/discourse/controllers/create-channel-modal.js
@@ -1,9 +1,9 @@
 import Controller from "@ember/controller";
 import discourseComputed from "discourse-common/utils/decorators";
+import I18n from "I18n";
 import ModalFunctionality from "discourse/mixins/modal-functionality";
 import { ajax } from "discourse/lib/ajax";
 import { action } from "@ember/object";
-import { bind, debounce } from "@ember/runloop";
 import { inject as service } from "@ember/service";
 
 export default Controller.extend(ModalFunctionality, {

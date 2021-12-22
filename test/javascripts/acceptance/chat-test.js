22c3d885fb975db5498547036b374f2393e506a6
Jordan Vidrine <jordan@jordanvidrine.com>
2021-12-22 13:28:55 -0600

    more

--- test/javascripts/acceptance/chat-test.js
+++ test/javascripts/acceptance/chat-test.js
@@ -1089,11 +1089,7 @@ acceptance(
       assert.notOk(query(".create-channel-modal-modal .btn.create").disabled);
 
       await click(".create-channel-modal-modal .btn.create");
-      const done = assert.async();
-      next(async () => {
-        assert.equal(currentURL(), "/chat/channel/88/Something");
-        done();
-      });
+      assert.equal(currentURL(), "/chat/channel/88/Something");
     });
   }
 );

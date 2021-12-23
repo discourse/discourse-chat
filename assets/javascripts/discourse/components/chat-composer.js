ba96f4a3b37559ba962330fd3aed772adac8937a
Natalie Tay <natalie.tay@discourse.org>
2021-12-23 17:20:46 +0800

    FEATURE: Show placeholders that tell the user who they're talking to (#384)
    * Favour name then username in placeholder

--- assets/javascripts/discourse/components/chat-composer.js
+++ assets/javascripts/discourse/components/chat-composer.js
@@ -27,11 +27,14 @@ const THROTTLE_MS = 150;
 let outsideToolbarClick;
 
 const toolbarButtons = [];
+
 export function addChatToolbarButton(toolbarButton) {
   toolbarButtons.push(toolbarButton);
 }
 
 export default Component.extend(TextareaTextManipulation, ComposerUploadUppy, {
+  chatChannel: null,
+
   chat: service(),
   classNames: ["tc-composer"],
   emojiStore: service("emoji-store"),
@@ -484,9 +487,31 @@ export default Component.extend(TextareaTextManipulation, ComposerUploadUppy, {
 
   @discourseComputed("previewing")
   placeholder(previewing) {
-    return I18n.t(
-      previewing ? "chat.placeholder_previewing" : "chat.placeholder"
-    );
+    return previewing
+      ? I18n.t("chat.placeholder_previewing")
+      : this.messageRecipient(this.chatChannel);
+  },
+
+  messageRecipient(chatChannel) {
+    if (chatChannel.chatable_type === "DirectMessageChannel") {
+      const directMessageRecipients = chatChannel.chatable.users;
+      if (
+        directMessageRecipients.length === 1 &&
+        directMessageRecipients[0].id === this.currentUser.id
+      ) {
+        return I18n.t("chat.placeholder_self");
+      }
+
+      return I18n.t("chat.placeholder", {
+        messageRecipient: directMessageRecipients
+          .map((u) => u.name || `@${u.username}`)
+          .join(", "),
+      });
+    } else {
+      return I18n.t("chat.placeholder", {
+        messageRecipient: `#${chatChannel.title}`,
+      });
+    }
   },
 
   @discourseComputed(

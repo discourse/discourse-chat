2a775d8cedf1b15107362c430815de3038fe2e8e
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-01-04 11:13:53 +0100

    actually not we need title

--- assets/javascripts/discourse/services/chat.js
+++ assets/javascripts/discourse/services/chat.js
@@ -478,7 +478,7 @@ export default Service.extend({
       .then(async () => {
         this._unsubscribeFromChatChannel(channel);
         return this._refreshChannels().then(() => {
-          return this.getIdealFirstChannelId().then((channelInfo) => {
+          return this.getIdealFirstChannelIdAndTitle().then((channelInfo) => {
             if (channelInfo) {
               return this.router.transitionTo(
                 "chat.channel",

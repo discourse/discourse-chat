8ff39a4909c4c28e6ef603e51c61e927e71b59f9
Nat <natalie.tay@discourse.org>
2022-01-05 18:44:34 +0800

    Properly shows multiple lines of cooked text which are appended to image

--- assets/javascripts/discourse/components/tc-message-collapser.js
+++ assets/javascripts/discourse/components/tc-message-collapser.js
@@ -2,6 +2,7 @@ import Component from "@ember/component";
 import { action, computed } from "@ember/object";
 import escape from "discourse-common/lib/escape";
 import domFromString from "discourse-common/lib/dom-from-string";
+import I18n from "I18n";
 
 export default Component.extend({
   collapsed: false,
@@ -11,6 +12,17 @@ export default Component.extend({
   @computed("cooked")
   get title() {
     return domFromString(this.cooked).dataset.youtubeTitle;
+  },
+
+  @computed("uploads")
+  get filename() {
+    if (this.uploads) {
+      if (this.uploads.length === 1) {
+        return this.uploads[0].original_filename;
+      } else {
+        return I18n.t(`chat.uploaded_files`, { count: this.uploads.length });
+      }
+    }
   },
 
   @computed("cooked")

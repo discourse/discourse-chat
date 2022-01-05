e5a320c72f636839c876f35a91c103fd004853a6
Nat <natalie.tay@discourse.org>
2022-01-05 15:43:01 +0800

    Revert "Remove uploads because NOT YET"
    This reverts commit 1bcf42a5d6883a5658a0bb79055ab668d29343dd.

--- assets/javascripts/discourse/components/tc-text.js
+++ assets/javascripts/discourse/components/tc-text.js
@@ -3,9 +3,10 @@ import { computed } from "@ember/object";
 
 export default Component.extend({
   cooked: null,
+  uploads: null,
   edited: false,
 
-  @computed("cooked")
+  @computed("cooked", "uploads")
   get isCollapsible() {
     return /^<div class="onebox lazyYT lazyYT-container"/.test(this.cooked);
   },

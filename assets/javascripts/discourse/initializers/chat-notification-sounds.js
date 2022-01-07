09e107e01c2c9aad94306541a1bcc0dbde68590f
Nat <natalie.tay@discourse.org>
2022-01-07 12:59:05 +0800

    Prevent audio from playing on top of each other CAUSING IT TO BE LOUD

--- assets/javascripts/discourse/initializers/chat-notification-sounds.js
+++ assets/javascripts/discourse/initializers/chat-notification-sounds.js
@@ -5,6 +5,12 @@ export const CHAT_SOUNDS = {
   ding: "/plugins/discourse-chat/audio/ding.mp3",
 };
 
+const MENTION = 29;
+const MESSAGE = 30;
+const CHAT_NOTIFICATION_TYPES = [MENTION, MESSAGE];
+
+const AUDIO_DEBOUNCE_TIMEOUT = 3000;
+
 export default {
   name: "chat-notification-sounds",
   initialize(container) {
@@ -13,13 +19,38 @@ export default {
       return;
     }
 
+    let canPlay = true;
+
+    function playAudio(user) {
+      try {
+        new Audio(CHAT_SOUNDS[user.chat_sound]).play();
+      } catch (error) {
+        if (error instanceof DOMException) {
+          // eslint-disable-next-line no-console
+          console.info(
+            "User needs to interact with DOM before we can play notification sounds"
+          );
+        } else {
+          throw error;
+        }
+      }
+    }
+
+    function playAudioWithDebounce(user) {
+      if (canPlay) {
+        canPlay = false;
+
+        setTimeout(() => {
+          canPlay = true;
+          playAudio(user);
+        }, AUDIO_DEBOUNCE_TIMEOUT);
+      }
+    }
+
     withPluginApi("0.12.1", (api) => {
       api.registerDesktopNotificationHandler((data, siteSettings, user) => {
-        // chat_mention and chat_message are notification_types of 29 and 30.
-        if ([29, 30].includes(data.notification_type)) {
-          const audio = new Audio(CHAT_SOUNDS[user.chat_sound]);
-          audio.volume = 0.8;
-          audio.play();
+        if (CHAT_NOTIFICATION_TYPES.includes(data.notification_type)) {
+          playAudioWithDebounce(user);
         }
       });
     });

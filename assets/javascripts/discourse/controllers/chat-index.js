8cb5d97d7643a7982c56b0b1a73ad4f394e70645
Mark VanLandingham <markvanlan@gmail.com>
2022-01-21 11:29:18 -0600

    Revert "FIX: Move empty state to browse page (#547)"
    This reverts commit 93e32f8b6b02473b0ce161d196c46cf21c1c06f8.

--- assets/javascripts/discourse/controllers/chat-index.js
+++ assets/javascripts/discourse/controllers/chat-index.js
@@ -1,2 +1,36 @@
 import Controller from "@ember/controller";
-export default Controller.extend();
+import showModal from "discourse/lib/show-modal";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+
+export default Controller.extend({
+  creatingDm: false,
+  router: service(),
+  blankPage: false,
+
+  @action
+  openFollowModal() {
+    showModal("chat-channel-settings");
+  },
+
+  @action
+  startCreatingDm() {
+    this.set("creatingDm", true);
+  },
+
+  @action
+  afterDmCreation(chatChannel) {
+    this.cancelDmCreation();
+    this.router.transitionTo("chat.channel", chatChannel.id, chatChannel.title);
+  },
+
+  @action
+  cancelDmCreation() {
+    this.set("creatingDm", false);
+  },
+
+  @action
+  selectChannel(channel) {
+    return this.router.transitionTo("chat.channel", channel.id, channel.title);
+  },
+});

7ad6369bdca2eb709b1c66e0ea7f3cf65925695f
Martin Brennan <martin@discourse.org>
2022-01-24 16:01:50 +1000

    DEV: Use core's new buildCustomMarkdownEngine API

--- assets/javascripts/lib/discourse-markdown/discourse-chat-transcript.js
+++ assets/javascripts/lib/discourse-markdown/discourse-chat-transcript.js
@@ -1,56 +1,57 @@
+const MARKDOWN_OVERRIDES = {
+  featuresOverride: [
+    "anchor",
+    "bbcode-block",
+    "bbcode-inline",
+    "category-hashtag",
+    "censored",
+    "discourse-local-dates",
+    "emoji",
+    // "emojiShortcuts",
+    "inlineEmoji",
+    "html-img",
+    "mentions",
+    "onebox",
+    "text-post-process",
+    "upload-protocol",
+    "watched-words",
+    "table",
+    "spoiler-alert",
+    "discourse-chat-transcript",
+  ],
+  markdownItRules: [
+    "autolink",
+    "list",
+    "backticks",
+    "newline",
+    "code",
+    "fence",
+    "table",
+    "linkify",
+    "link",
+    "strikethrough",
+    "blockquote",
+    "emphasis",
+  ],
+};
+let customMarkdownCookFn;
+
 const chatTranscriptRule = {
   tag: "chat",
-
-  replace: function (state, tagInfo, content) {
-    const token = state.push("html_raw", "", 0);
-    token.discourseOpts = {
-      rerender: true,
-      featuresOverride: [
-        "anchor",
-        "bbcode-block",
-        "bbcode-inline",
-        "category-hashtag",
-        "censored",
-        "discourse-local-dates",
-        "emoji",
-        // "emojiShortcuts",
-        "inlineEmoji",
-        "html-img",
-        "mentions",
-        "onebox",
-        "text-post-process",
-        "upload-protocol",
-        "watched-words",
-        "table",
-        "spoiler-alert",
-        "discourse-chat-transcript",
-      ],
-      markdownItRules: [
-        "autolink",
-        // "list",
-        "backticks",
-        "newline",
-        "code",
-        "fence",
-        "table",
-        "linkify",
-        "link",
-        "strikethrough",
-        "blockquote",
-        "emphasis",
-      ],
-    };
-    token.content = `[chatbaked quote="${tagInfo.attrs.quote}"]\n${content}\n[/chatbaked]`;
-    return true;
-  },
-};
-
-const chatBakedTranscriptRule = {
-  tag: "chatbaked",
 
   before: function (state) {
     let wrapperDivToken = state.push("div_chat_transcript_wrap", "div", 1);
     wrapperDivToken.attrs = [["class", "discourse-chat-transcript"]];
+  },
+
+  replace: function (state, tagInfo, content) {
+    let wrapperDivToken = state.push("div_chat_transcript_wrap", "div", 1);
+    wrapperDivToken.attrs = [["class", "discourse-chat-transcript"]];
+    const token = state.push("html_raw", "", 0);
+    token.content = customMarkdownCookFn(content);
+    state.push("html_raw", "", -1);
+    state.push("div_chat_transcript_wrap", "div", -1);
+    return true;
   },
 
   after: function (state) {
@@ -77,10 +78,12 @@ export function setup(helper) {
         "discourse-chat-transcript",
         chatTranscriptRule
       );
-      md.block.bbcode.ruler.push(
-        "discourse-chat-rare-transcript",
-        chatBakedTranscriptRule
-      );
     }
+  });
+
+  helper.buildCustomMarkdownEngine((build) => {
+    build(MARKDOWN_OVERRIDES).then((customMarkdownEngineRenderFn) => {
+      customMarkdownCookFn = customMarkdownEngineRenderFn;
+    });
   });
 }

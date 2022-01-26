65f893f1e1a39f6a0f29b70ef1e1da7d036f6c70
Mark VanLandingham <markvanlan@gmail.com>
2022-01-26 11:16:51 -0600

    FIX: Prevent default keyboard shortcuts

--- assets/javascripts/discourse/initializers/chat-keyboard-shortcuts.js
+++ assets/javascripts/discourse/initializers/chat-keyboard-shortcuts.js
@@ -1,39 +1,30 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 import showModal from "discourse/lib/show-modal";
 
-function openChannelSelector(appEvents) {
-  if (document.getElementById("chat-channel-selector-modal-inner")) {
-    appEvents.trigger("chat-channel-selector-modal:close");
-  } else {
-    showModal("chat-channel-selector-modal");
-  }
-}
-
 export default {
   name: "chat-keyboard-shortcuts",
   initialize(container) {
-    const appEvents = container.lookup("service:app-events");
     const chatService = container.lookup("service:chat");
-
     if (!chatService.userCanChat) {
       return;
     }
 
+    const appEvents = container.lookup("service:app-events");
+    const openChannelSelector = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      if (document.getElementById("chat-channel-selector-modal-inner")) {
+        appEvents.trigger("chat-channel-selector-modal:close");
+      } else {
+        showModal("chat-channel-selector-modal");
+      }
+    };
+
     withPluginApi("0.12.1", (api) => {
-      api.addKeyboardShortcut(
-        "command+k",
-        () => {
-          openChannelSelector(appEvents);
-        },
-        { global: true }
-      );
-      api.addKeyboardShortcut(
-        "ctrl+k",
-        () => {
-          openChannelSelector(appEvents);
-        },
-        { global: true }
-      );
+      api.addKeyboardShortcut("command+k", openChannelSelector, {
+        global: true,
+      });
+      api.addKeyboardShortcut("ctrl+k", openChannelSelector, { global: true });
     });
   },
 };

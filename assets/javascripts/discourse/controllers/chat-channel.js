a7d153eb3a55196da20cb423be610ce3e6630ed4
Mark VanLandingham <markvanlan@gmail.com>
2022-02-01 13:19:15 -0600

    remove stuff

--- assets/javascripts/discourse/controllers/chat-channel.js
+++ assets/javascripts/discourse/controllers/chat-channel.js
@@ -1,9 +1,5 @@
 import Controller from "@ember/controller";
-import { action } from "@ember/object";
-import { ajax } from "discourse/lib/ajax";
-import { inject as service } from "@ember/service";
 
 export default Controller.extend({
   queryParams: ["messageId"],
-  chat: service(),
 });

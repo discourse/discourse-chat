71fdcf3c1d7aca172b7f0ac695e58e676a1c9fd9
Mark VanLandingham <markvanlan@gmail.com>
2022-02-16 16:02:37 -0600

    move n-1

--- app/serializers/chat_in_reply_to_serializer.rb
+++ app/serializers/chat_in_reply_to_serializer.rb
@@ -2,6 +2,7 @@
 
 class ChatInReplyToSerializer < ApplicationSerializer
   has_one :user, serializer: BasicUserSerializer, embed: :objects
+  has_one :chat_webhook_event, serializer: ChatWebhookEventSerializer, embed: :objects
 
   attributes :id, :cooked, :excerpt
 end

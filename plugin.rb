c6f498e8a3066b3491aa23a412d25dc7707e9837
Jordan Vidrine <30537603+jordanvidrine@users.noreply.github.com>
2022-02-23 12:14:50 -0600

    UX: Add visual element when dragging files to upload into chat (#631)

--- plugin.rb
+++ plugin.rb
@@ -26,6 +26,10 @@ register_svg_icon "comment-slash"
 register_svg_icon "hashtag"
 register_svg_icon "lock"
 
+register_svg_icon "file-audio"
+register_svg_icon "file-video"
+register_svg_icon "file-image"
+
 # route: /admin/plugins/chat
 add_admin_route 'chat.admin.title', 'chat'
 

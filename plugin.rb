ad01dee8730014ff4a619698550594c25be8df5a
Jordan Vidrine <jordan@jordanvidrine.com>
2022-02-23 11:57:14 -0600

    more styling

--- plugin.rb
+++ plugin.rb
@@ -26,9 +26,6 @@ register_svg_icon "comment-slash"
 register_svg_icon "hashtag"
 register_svg_icon "lock"
 
-register_svg_icon "volume"
-register_svg_icon "tv-alt"
-
 register_svg_icon "file-audio"
 register_svg_icon "file-video"
 register_svg_icon "file-image"

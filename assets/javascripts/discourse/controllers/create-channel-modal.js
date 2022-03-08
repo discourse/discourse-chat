1cc115042596ea266806c8f348f0b942308b7707
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-03-08 16:06:13 +0100

    DEV: avoids implicit conversion from boolean to number or string

--- assets/javascripts/discourse/controllers/create-channel-modal.js
+++ assets/javascripts/discourse/controllers/create-channel-modal.js
@@ -7,6 +7,7 @@ import ModalFunctionality from "discourse/mixins/modal-functionality";
 import { ajax } from "discourse/lib/ajax";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
+import { isEmpty } from "@ember/utils";
 
 export default Controller.extend(ModalFunctionality, {
   chat: service(),
@@ -32,7 +33,7 @@ export default Controller.extend(ModalFunctionality, {
 
   @discourseComputed("type", "topic", "category", "name")
   createDisabled(type, topic, category, name) {
-    return !this.entitySelected || !name?.length > 0;
+    return !this.entitySelected || isEmpty(name);
   },
 
   @discourseComputed("category")

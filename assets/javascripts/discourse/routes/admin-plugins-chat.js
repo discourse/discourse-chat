0df7f8ce931a7da65a4794a032852329448dfac7
romanrizzi <rizziromanalejandro@gmail.com>
2022-03-11 10:31:13 -0300

    FIX: Chat channel selector was broken for incoming webhooks.
    A follow-up to #644. When creating or editing an incoming webhook, the channel selector doesn't work unless each `chat_channel` is a ChatChannel object.

--- assets/javascripts/discourse/routes/admin-plugins-chat.js
+++ assets/javascripts/discourse/routes/admin-plugins-chat.js
@@ -1,5 +1,6 @@
 import DiscourseRoute from "discourse/routes/discourse";
 import EmberObject from "@ember/object";
+import ChatChannel from "discourse/plugins/discourse-chat/discourse/models/chat-channel";
 import { ajax } from "discourse/lib/ajax";
 
 export default DiscourseRoute.extend({
@@ -12,6 +13,11 @@ export default DiscourseRoute.extend({
       model.incoming_chat_webhooks = model.incoming_chat_webhooks.map(
         (webhook) => EmberObject.create(webhook)
       );
+
+      model.chat_channels = model.chat_channels.map((channel) => {
+        return ChatChannel.create(channel);
+      });
+
       return model;
     });
   },

a85596b50d6d79031c7a3e7ba8456c5ff53428ba
Martin Brennan <martin@discourse.org>
2022-03-11 05:48:00 +0000

    DEV: Fix flaky again

--- assets/javascripts/discourse/initializers/chat-setup.js
+++ assets/javascripts/discourse/initializers/chat-setup.js
@@ -5,7 +5,7 @@ import { bind } from "discourse-common/utils/decorators";
 export default {
   name: "chat-setup",
   initialize(container) {
-    const currentUser = container.lookup("current-user:main");
+    this.currentUser = container.lookup("current-user:main");
     this.chatService = container.lookup("service:chat");
 
     withPluginApi("0.12.1", (api) => {
@@ -13,8 +13,12 @@ export default {
       // of whether the current user has chat enabled
       api.decorateCookedElement(
         (elem) => {
+          if (!this.currentUser) {
+            this.currentUser = container.lookup("current-user:main");
+          }
+
           const currentUserTimezone =
-            currentUser?.resolvedTimezone(currentUser);
+            this.currentUser?.resolvedTimezone(this.currentUser);
           const chatTranscriptElements = elem.querySelectorAll(
             ".discourse-chat-transcript"
           );
@@ -45,7 +49,7 @@ export default {
       document.body.classList.add("chat-enabled");
 
       if (api.container.lookup("site:main").mobileView) {
-        currentUser.chat_isolated = false;
+        this.currentUser.chat_isolated = false;
       }
 
       this.chatService.getChannels();

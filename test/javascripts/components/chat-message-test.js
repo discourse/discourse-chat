86060b056325dcb21ab1e0d40c68f6dd3c16f7a5
Mark VanLandingham <markvanlan@gmail.com>
2022-03-17 13:17:52 -0500

    Add test

---
+++ test/javascripts/components/chat-message-test.js
@@ -0,0 +1,92 @@
+import componentTest, {
+  setupRenderingTest,
+} from "discourse/tests/helpers/component-test";
+import {
+  discourseModule,
+  exists,
+  query,
+} from "discourse/tests/helpers/qunit-helpers";
+import hbs from "htmlbars-inline-precompile";
+import ChatChannel from "discourse/plugins/discourse-chat/discourse/models/chat-channel";
+import EmberObject from "@ember/object";
+import I18n from "I18n";
+
+discourseModule("Discourse Chat | Component | chat-message", function (hooks) {
+  setupRenderingTest(hooks);
+  const template = hbs`{{chat-message
+      message=message
+      canInteractWithChat=canInteractWithChat
+      details=this.details
+      chatChannel=chatChannel
+      setReplyTo=setReplyTo
+      replyMessageClicked=replyMessageClicked
+      editButtonClicked=editButtonClicked
+      afterExpand=decorateMessages
+      selectingMessages=selectingMessages
+      onStartSelectingMessages=onStartSelectingMessages
+      onSelectMessage=onSelectMessage
+      bulkSelectMessages=bulkSelectMessages
+      fullPage=fullPage
+      afterReactionAdded=reStickScrollIfNeeded
+    }}`;
+
+  const chatChannel = ChatChannel.create({
+    chatable: { id: 1 },
+    chatable_type: "Category",
+    id: 9,
+    title: "Site",
+    unread_count: 0,
+    muted: false,
+  });
+
+  componentTest("Message with deleted user", {
+    template,
+    async beforeEach() {
+      this.setProperties({
+        message: EmberObject.create({
+          id: 178,
+          message: "from deleted user",
+          cooked: "<p>from deleted user</p>",
+          excerpt: "<p>from deleted user</p>",
+          action_code: null,
+          created_at: "2021-07-22T08:14:16.950Z",
+          flag_count: 0,
+          user: null,
+        }),
+        canInteractWithChat: true,
+        details: {
+          chat_channel_id: chatChannel.id,
+          chatable_type: chatChannel.chatable_type,
+          can_delete_self: true,
+          can_delete_others: true,
+          can_flag: true,
+          user_silenced: false,
+          can_moderate: true,
+        },
+        chatChannel,
+        setReplyTo: () => {},
+        replyMessageClicked: () => {},
+        editButtonClicked: () => {},
+        afterExpand: () => {},
+        selectingMessages: false,
+        onStartSelectingMessages: () => {},
+        onSelectMessage: () => {},
+        bulkSelectMessages: () => {},
+        fullPage: false,
+        afterReactionAdded: () => {},
+      });
+    },
+
+    async test(assert) {
+      assert.equal(
+        query(
+          ".chat-message .chat-message-sender-data .username"
+        ).innerText.trim(),
+        I18n.t("chat.user_deleted")
+      );
+      assert.ok(
+        exists(".chat-message .chat-emoji-avatar .emoji[title='wastebasket']")
+      );
+    },
+  });
+});

3809859593b8ad00406c5bbeba793b3784833045
Mark VanLandingham <markvanlan@gmail.com>
2022-03-22 09:32:31 -0500

    merge

--- plugin.rb
+++ plugin.rb
@@ -182,6 +182,21 @@ after_initialize do
       object.custom_fields[DiscourseChat::HAS_CHAT_ENABLED]
   end
 
+  add_to_serializer(:post, :chat_connection) do
+    if object.chat_message_post_connections&.first&.chat_message
+      {
+        chat_channel_id: object.chat_message_post_connections.first.chat_message.chat_channel_id,
+        chat_message_ids: object.chat_message_post_connections.map(&:chat_message_id)
+      }
+    end
+  end
+
+  add_to_serializer(:user_card, :can_chat) do
+    SiteSetting.chat_enabled &&
+      scope.can_chat?(scope.user) &&
+      scope.can_chat?(object)
+  end
+
   add_to_serializer(:current_user, :can_chat) do
     true
   end

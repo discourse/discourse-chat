58c069c9a7f7655a8570003dbee70a986c18bf93
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-03-25 12:30:24 +0100

    UX: implements using long-press to trigger mobile menu

--- assets/javascripts/discourse/components/chat-message-actions-mobile.js
+++ assets/javascripts/discourse/components/chat-message-actions-mobile.js
@@ -1,13 +1,34 @@
 import Component from "@ember/component";
 import { later } from "@ember/runloop";
+import { action } from "@ember/object";
 
 export default Component.extend({
   tagName: "",
+  isExpanded: false,
+  messageActions: null,
 
   didInsertElement() {
     this._super(...arguments);
 
     later(this._addFadeIn);
+  },
+
+  @action
+  expandReply(event) {
+    event.stopPropagation();
+    this.set("isExpanded", true);
+  },
+
+  @action
+  collapseMenu(event) {
+    event.stopPropagation();
+    this.onHoverMessage();
+  },
+
+  @action
+  actAndCloseMenu(fn) {
+    fn?.();
+    this.onHoverMessage();
   },
 
   _addFadeIn() {

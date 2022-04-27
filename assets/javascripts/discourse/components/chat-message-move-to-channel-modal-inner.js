6f9f4f3a632ffa0f9aa47d49087837bcee806ca1
Martin Brennan <martin@discourse.org>
2022-04-27 17:01:23 +1000

    DEV: Lint fix and hide DM channels in move channel modal

--- assets/javascripts/discourse/components/chat-message-move-to-channel-modal-inner.js
+++ assets/javascripts/discourse/components/chat-message-move-to-channel-modal-inner.js
@@ -1,9 +1,4 @@
 import Component from "@ember/component";
-import { isTesting } from "discourse-common/config/environment";
-import { later } from "@ember/runloop";
-import { isEmpty } from "@ember/utils";
-import I18n from "I18n";
-import discourseComputed from "discourse-common/utils/decorators";
 import { action, computed } from "@ember/object";
 import { ajax } from "discourse/lib/ajax";
 import { inject as service } from "@ember/service";
@@ -34,9 +29,9 @@ export default class MoveToChannelModalInner extends Component {
     if (!this.channels) {
       return [];
     }
-    return this.channels.publicChannels
-      .concat(this.channels.directMessageChannels)
-      .reject((chan) => chan.id === this.sourceChannel.id);
+    return this.channels.publicChannels.reject(
+      (chan) => chan.id === this.sourceChannel.id
+    );
   }
 
   get selectedMessageCount() {

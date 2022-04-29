e7645c465d3e744298ee65032c7d3cb5818b5683
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-04-29 16:26:53 +0200

    DEV: implement support for dev:populate (#823)
    Note this also adds the following tasks:
    - chat:populate
    - chat:message:populate

---
+++ lib/discourse_dev/public_channel.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'discourse_dev/record'
+require 'faker'
+
+module DiscourseDev
+  class PublicChannel < Record
+    def initialize
+      super(::ChatChannel, 5)
+    end
+
+    def data
+      chatable = nil
+      if Faker::Boolean.boolean(true_ratio: 0.5)
+        chatable = Category.random
+      else
+        chatable = Record.random(::Topic)
+      end
+
+      {
+        chatable: chatable,
+        description: Faker::Lorem.paragraph,
+        user_count: 1,
+        name: Faker::Company.name,
+        created_at: Faker::Time.between(from: DiscourseDev.config.start_date, to: DateTime.now),
+      }
+    end
+
+    def create!
+      super do |channel|
+        Faker::Number.between(from: 5, to: 10).times do
+          if Faker::Boolean.boolean(true_ratio: 0.5)
+            admin_username = DiscourseDev::Config.new.config[:admin][:username]
+            admin_user = ::User.find_by(username: admin_username)
+          end
+
+          ::UserChatChannelMembership.find_or_create_by!(
+            user: admin_user || User.new.create!,
+            chat_channel: channel,
+            following: true,
+          )
+        end
+      end
+    end
+  end
+end

3b4d958f4e0519458cceb7e34f0775f297893948
Blake Erickson <o.blakeerickson@gmail.com>
2022-05-24 17:41:28 -0600

    prettier fix for test

--- test/javascripts/unit/lib/chat-window-store-test.js
+++ test/javascripts/unit/lib/chat-window-store-test.js
@@ -1,32 +1,27 @@
 import { discourseModule } from "discourse/tests/helpers/qunit-helpers";
 import { test } from "qunit";
 
-discourseModule(
-  "Discourse Chat | Unit | chat-window-store",
-  function (hooks) {
-    hooks.beforeEach(function () {
-      this.chatWindowStore = this.container.lookup(
-        "service:chat-window-store"
-      );
-    });
+discourseModule("Discourse Chat | Unit | chat-window-store", function (hooks) {
+  hooks.beforeEach(function () {
+    this.chatWindowStore = this.container.lookup("service:chat-window-store");
+  });
 
-    test("defaults", function (assert) {
-      assert.strictEqual(this.chatWindowStore.fullPage, false);
-    });
+  test("defaults", function (assert) {
+    assert.strictEqual(this.chatWindowStore.fullPage, false);
+  });
 
-    test("fullPage", function (assert) {
-      this.chatWindowStore.set("fullPage", true);
-      assert.strictEqual(this.chatWindowStore.fullPage, true);
-    });
+  test("fullPage", function (assert) {
+    this.chatWindowStore.set("fullPage", true);
+    assert.strictEqual(this.chatWindowStore.fullPage, true);
+  });
 
-    test("fullPageFalse", function (assert) {
-      this.chatWindowStore.set("fullPage", false);
-      assert.strictEqual(this.chatWindowStore.fullPage, false);
-    });
+  test("fullPageFalse", function (assert) {
+    this.chatWindowStore.set("fullPage", false);
+    assert.strictEqual(this.chatWindowStore.fullPage, false);
+  });
 
-    test("fullPageNull", function (assert) {
-      this.chatWindowStore.set("fullPage", null);
-      assert.strictEqual(this.chatWindowStore.fullPage, false);
-    });
-  }
-);
+  test("fullPageNull", function (assert) {
+    this.chatWindowStore.set("fullPage", null);
+    assert.strictEqual(this.chatWindowStore.fullPage, false);
+  });
+});

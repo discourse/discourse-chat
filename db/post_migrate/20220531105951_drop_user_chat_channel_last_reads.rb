0ec432e73a2973f5b520bec98041e5956577c54d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-05-31 13:03:30 +0200

    linting

--- db/post_migrate/20220531105951_drop_user_chat_channel_last_reads.rb
+++ db/post_migrate/20220531105951_drop_user_chat_channel_last_reads.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DropUserChatChannelLastReads < ActiveRecord::Migration[7.0]
   def up
     # usage has been dropped in https://github.com/discourse/discourse-chat/commit/1c110b71b28411dc7ac3ab9e3950e0bbf38d7970

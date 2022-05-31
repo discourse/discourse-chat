ea7bad1792a8182502c5ed1f52add2b72be6ed60
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-05-31 13:00:49 +0200

    drops table in different migration

---
+++ db/post_migrate/20220531105951_drop_user_chat_channel_last_reads.rb
@@ -0,0 +1,7 @@
+class DropUserChatChannelLastReads < ActiveRecord::Migration[7.0]
+  def up
+    # usage has been dropped in https://github.com/discourse/discourse-chat/commit/1c110b71b28411dc7ac3ab9e3950e0bbf38d7970
+    # but apparently table never got dropped
+    drop_table :user_chat_channel_last_reads
+  end
+end

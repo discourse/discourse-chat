a0890ee2ae9ac7676d5002b1a472a907a04b936c
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-06-30 12:03:32 +0200

    FIX: sidebar channels should be shown at all time (#975)

--- assets/javascripts/discourse/components/sidebar-channels.js
+++ assets/javascripts/discourse/components/sidebar-channels.js
@@ -9,9 +9,9 @@ export default class SidebarChannels extends Component {
   @service chat;
   @service router;
 
-  @computed("chat.{userCanChat,isFullPageChat}")
+  @computed("chat.userCanChat")
   get isDisplayed() {
-    return this.chat.userCanChat && this.chat.fullScreenChatOpen;
+    return this.chat.userCanChat;
   }
 
   @action

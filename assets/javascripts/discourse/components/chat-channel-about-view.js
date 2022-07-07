458f25368bc2d80c385ce010b958a6ae4c56fe17
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-07-07 11:25:11 +0200

    UX: moves join/leave button to about tab (#1003)

--- assets/javascripts/discourse/components/chat-channel-about-view.js
+++ assets/javascripts/discourse/components/chat-channel-about-view.js
@@ -1,8 +1,47 @@
 import Component from "@ember/component";
+import { action } from "@ember/object";
+import ChatApi from "discourse/plugins/discourse-chat/discourse/lib/chat-api";
+import { popupAjaxError } from "discourse/lib/ajax-error";
+import { inject as service } from "@ember/service";
 
 export default class ChatChannelAboutView extends Component {
   tagName = "";
   channel = null;
   onEditChatChannelTitle = null;
   onEditChatChannelDescription = null;
+  isLoading = false;
+
+  @service chat;
+
+  @action
+  onJoinChannel() {
+    this.set("isLoading", true);
+
+    return ChatApi.followChatChannel(this.channel.id)
+      .then((membership) => {
+        this.channel.set("following", true);
+        this.channel.set("memberships_count", membership.user_count);
+
+        return this.chat
+          .forceRefreshChannels()
+          .then(() => this.chat.openChannel(this.channel));
+      })
+      .catch(popupAjaxError);
+  }
+
+  @action
+  onLeaveChannel() {
+    this.set("isLoading", true);
+
+    return ChatApi.unfollowChatChannel(this.channel.id)
+      .then((membership) => {
+        this.channel.set("following", false);
+        this.channel.set("memberships_count", membership.user_count);
+
+        return this.chat
+          .forceRefreshChannels()
+          .then(() => this.chat.openChannel(this.channel));
+      })
+      .catch(popupAjaxError);
+  }
 }

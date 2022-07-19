0bc01f033b740fe0fdf1ae06fe089eae16a6096a
Jarek Radosz <jradosz@gmail.com>
2022-07-19 20:19:23 +0200

    DEV: Cleanup fabricators code, fix linting

--- test/javascripts/helpers/fabricators.js
+++ test/javascripts/helpers/fabricators.js
@@ -1,10 +1,10 @@
 import ChatChannel, {
   CHATABLE_TYPES,
 } from "discourse/plugins/discourse-chat/discourse/models/chat-channel";
-import { Fabricator } from "./fabricator";
 import EmberObject from "@ember/object";
+import { Fabricator } from "./fabricator";
 
-const userFabricator = Fabricator({
+const userFabricator = Fabricator(EmberObject, {
   id: 1,
   username: "hawk",
   name: null,
@@ -12,46 +12,38 @@ const userFabricator = Fabricator({
     "https://avatars.discourse.org/v3/letter/t/41988e/{size}.png",
 });
 
-const categoryChatableFabricator = Fabricator({
+const categoryChatableFabricator = Fabricator(EmberObject, {
   id: 1,
   color: "D56353",
   read_restricted: false,
   name: "My category",
 });
 
-const directChannelChatableFabricator = Fabricator({
+const directChannelChatableFabricator = Fabricator(EmberObject, {
   users: [userFabricator({ id: 1, username: "bob" })],
 });
 
-const chatChannelMessageFabricator = Fabricator({
-  id: 1,
-  chat_channel_id: 1,
-  user_id: 1,
-  cooked: "This is a test message",
-});
-
-const directMessageChannelFabricator = Fabricator({
-  id: 1,
-  chatable_type: CHATABLE_TYPES.directMessageChannel,
-  status: "open",
-  chatable: directChannelChatableFabricator(),
-  __model: "ChatChannel",
-});
-
-const chatChannelFabricator = Fabricator({
-  id: 1,
-  chatable_type: CHATABLE_TYPES.categoryChannel,
-  status: "open",
-  title: "My category title",
-  name: "My category name",
-  chatable: categoryChatableFabricator(),
-  __model: "ChatChannel",
-});
-
 export default {
-  chatChannel: (options) => chatChannelFabricator(options),
-  directMessageChatChannel: (options) =>
-    directMessageChannelFabricator(options),
-  chatChannelMessage: (options) =>
-    EmberObject.create(chatChannelMessageFabricator(options)),
+  chatChannel: Fabricator(ChatChannel, {
+    id: 1,
+    chatable_type: CHATABLE_TYPES.categoryChannel,
+    status: "open",
+    title: "My category title",
+    name: "My category name",
+    chatable: categoryChatableFabricator(),
+  }),
+
+  chatChannelMessage: Fabricator(EmberObject, {
+    id: 1,
+    chat_channel_id: 1,
+    user_id: 1,
+    cooked: "This is a test message",
+  }),
+
+  directMessageChatChannel: Fabricator(ChatChannel, {
+    id: 1,
+    chatable_type: CHATABLE_TYPES.directMessageChannel,
+    status: "open",
+    chatable: directChannelChatableFabricator(),
+  }),
 };

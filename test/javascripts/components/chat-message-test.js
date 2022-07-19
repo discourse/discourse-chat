ace2a8d94197b1e009d88de627eff98bd66b7004
Martin Brennan <martin@discourse.org>
2022-07-19 15:15:57 +1000

    DEV: Add tests for hidden/deleted messages

--- test/javascripts/components/chat-message-test.js
+++ test/javascripts/components/chat-message-test.js
@@ -1,6 +1,7 @@
-import componentTest, {
-  setupRenderingTest,
-} from "discourse/tests/helpers/component-test";
+import { setupRenderingTest } from "discourse/tests/helpers/component-test";
+import User from "discourse/models/user";
+import { render } from "@ember/test-helpers";
+import ChatMessage from "discourse/plugins/discourse-chat/discourse/models/chat-message";
 import {
   discourseModule,
   exists,
@@ -10,9 +11,58 @@ import hbs from "htmlbars-inline-precompile";
 import ChatChannel from "discourse/plugins/discourse-chat/discourse/models/chat-channel";
 import EmberObject from "@ember/object";
 import I18n from "I18n";
+import { test } from "qunit";
 
 discourseModule("Discourse Chat | Component | chat-message", function (hooks) {
   setupRenderingTest(hooks);
+
+  function generateMessageProps(messageData = {}) {
+    const chatChannel = ChatChannel.create({
+      chatable: { id: 1 },
+      chatable_type: "Category",
+      id: 9,
+      title: "Site",
+      unread_count: 0,
+      muted: false,
+    });
+    return {
+      message: ChatMessage.create(
+        Object.assign(
+          {
+            id: 178,
+            message: "from deleted user",
+            cooked: "<p>from deleted user</p>",
+            excerpt: "<p>from deleted user</p>",
+            created_at: "2021-07-22T08:14:16.950Z",
+            flag_count: 0,
+            user: User.create({ username: "someguy", id: 1424 }),
+            edited: false,
+          },
+          messageData
+        )
+      ),
+      canInteractWithChat: true,
+      details: {
+        can_delete_self: true,
+        can_delete_others: true,
+        can_flag: true,
+        user_silenced: false,
+        can_moderate: true,
+      },
+      chatChannel,
+      setReplyTo: () => {},
+      replyMessageClicked: () => {},
+      editButtonClicked: () => {},
+      afterExpand: () => {},
+      selectingMessages: false,
+      onStartSelectingMessages: () => {},
+      onSelectMessage: () => {},
+      bulkSelectMessages: () => {},
+      fullPage: false,
+      afterReactionAdded: () => {},
+      onHoverMessage: () => {},
+    };
+  }
 
   const template = hbs`{{chat-message
       message=message
@@ -31,94 +81,44 @@ discourseModule("Discourse Chat | Component | chat-message", function (hooks) {
       afterReactionAdded=reStickScrollIfNeeded
     }}`;
 
-  componentTest("Message with deleted user", {
-    template,
-
-    async beforeEach() {
-      const chatChannel = ChatChannel.create({
-        chatable: { id: 1 },
-        chatable_type: "Category",
-        id: 9,
-        title: "Site",
-        unread_count: 0,
-        muted: false,
-      });
-
-      this.setProperties({
-        message: EmberObject.create({
-          id: 178,
-          message: "from deleted user",
-          cooked: "<p>from deleted user</p>",
-          excerpt: "<p>from deleted user</p>",
-          created_at: "2021-07-22T08:14:16.950Z",
-          flag_count: 0,
-          user: null,
-        }),
-        canInteractWithChat: true,
-        details: {
-          can_delete_self: true,
-          can_delete_others: true,
-          can_flag: true,
-          user_silenced: false,
-          can_moderate: true,
-        },
-        chatChannel,
-        setReplyTo: () => {},
-        replyMessageClicked: () => {},
-        editButtonClicked: () => {},
-        afterExpand: () => {},
-        selectingMessages: false,
-        onStartSelectingMessages: () => {},
-        onSelectMessage: () => {},
-        bulkSelectMessages: () => {},
-        fullPage: false,
-        afterReactionAdded: () => {},
-        onHoverMessage: () => {},
-      });
-    },
-
-    async test(assert) {
-      assert.equal(
-        query(".chat-message-info__username__name").innerText.trim(),
-        I18n.t("chat.user_deleted")
-      );
-      assert.ok(
-        exists(".chat-message .chat-emoji-avatar .emoji[title='wastebasket']")
-      );
-    },
+  test("Message with deleted user", async function (assert) {
+    this.setProperties(generateMessageProps({ user: null }));
+    await render(template);
+    assert.equal(
+      query(".chat-message-info__username__name").innerText.trim(),
+      I18n.t("chat.user_deleted"),
+      "shows the user_deleted text for the username"
+    );
+    assert.ok(
+      exists(".chat-message .chat-emoji-avatar .emoji[title='wastebasket']"),
+      "shows the wastebasket avatar"
+    );
   });
 
-  componentTest("Message with edits", {
-    template,
+  test("Message with edits", async function (assert) {
+    this.setProperties(generateMessageProps({ edited: true }));
+    await render(template);
+    assert.ok(
+      exists(".chat-message-edited"),
+      "has the correct edited css class"
+    );
+  });
 
-    async beforeEach() {
-      this.setProperties({
-        message: EmberObject.create({
-          id: 178,
-          message: "tomtom",
-          cooked: "tomtom",
-          excerpt: "tomtom",
-          created_at: "2021-07-22T08:14:16.950Z",
-          flag_count: 0,
-          user: null,
-          edited: true,
-        }),
-        setReplyTo: () => {},
-        replyMessageClicked: () => {},
-        editButtonClicked: () => {},
-        afterExpand: () => {},
-        selectingMessages: false,
-        onStartSelectingMessages: () => {},
-        onSelectMessage: () => {},
-        bulkSelectMessages: () => {},
-        fullPage: false,
-        afterReactionAdded: () => {},
-        onHoverMessage: () => {},
-      });
-    },
+  test("Deleted message", async function (assert) {
+    this.setProperties(generateMessageProps({ deleted_at: moment() }));
+    await render(template);
+    assert.ok(
+      exists(".chat-message-deleted .chat-message-expand"),
+      "has the correct deleted css class and expand button within"
+    );
+  });
 
-    async test(assert) {
-      assert.ok(exists(".chat-message-edited"));
-    },
+  test("Hidden message", async function (assert) {
+    this.setProperties(generateMessageProps({ hidden: true }));
+    await render(template);
+    assert.ok(
+      exists(".chat-message-hidden .chat-message-expand"),
+      "has the correct hidden css class and expand button within"
+    );
   });
 });

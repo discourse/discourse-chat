2bcb2135904da493c38bee3081d2ccca3f1fdaf1
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-07-24 18:59:31 +0200

    UX: improves state of the composer on mobile

---
+++ assets/javascripts/discourse/components/chat-vh.js
@@ -0,0 +1,46 @@
+import { bind } from "discourse-common/utils/decorators";
+import Component from "@ember/component";
+import { throttle } from "@ember/runloop";
+
+const CSS_VAR = "--chat-vh";
+
+export default class ChatVh extends Component {
+  tagName = "";
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    this.setVH();
+
+    (window?.visualViewport || window).addEventListener(
+      "resize",
+      this.setVHThrottler,
+      false
+    );
+  }
+
+  willDestroyElement() {
+    this._super(...arguments);
+
+    (window?.visualViewport || window).removeEventListener(
+      "resize",
+      this.setVHThrottler
+    );
+  }
+
+  @bind
+  setVH() {
+    if (this.isDestroying || this.isDestroyed) {
+      return;
+    }
+
+    const composerVH =
+      (window.visualViewport?.height || window.innerHeight) * 0.01;
+    document.documentElement.style.setProperty(CSS_VAR, `${composerVH}px`);
+  }
+
+  @bind
+  setVHThrottler() {
+    throttle(this, this.setVH, 100);
+  }
+}

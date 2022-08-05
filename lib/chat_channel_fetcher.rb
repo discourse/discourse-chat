a9c60ebc37d49bb70fe5735278737dbb89617e4f
Martin Brennan <martin@discourse.org>
2022-08-05 16:15:04 +1000

    DEV: Move mention and unread fetching into own method

--- lib/chat_channel_fetcher.rb
+++ lib/chat_channel_fetcher.rb
@@ -98,34 +98,7 @@ module DiscourseChat::ChatChannelFetcher
     options[:offset] = [options[:offset].to_i, 0].max
 
     channels = channels.limit(options[:limit]).offset(options[:offset])
-
-    ###
-    # TODO (martin)
-    # This rats nest needs to be purged with cleansing fire.
-    #
-    # Suffer not the xenos to live.
-    #
-    unread_counts_per_channel = unread_counts(channels, guardian.user.id)
-    mention_notifications =
-      Notification.unread.where(
-        user_id: guardian.user.id,
-        notification_type: Notification.types[:chat_mention],
-      )
-    mention_notification_data = mention_notifications.map { |m| JSON.parse(m.data) }
-    channels.each do |channel|
-      membership = memberships.find { |m| m.chat_channel_id == channel.id }
-      if membership
-        membership.unread_mentions =
-          mention_notification_data.count do |data|
-            data["chat_channel_id"] == channel.id &&
-              data["chat_message_id"] > (membership.last_read_message_id || 0)
-          end
-        membership.unread_count = unread_counts_per_channel[channel.id] if !membership.muted
-      end
-    end
-    #
-    #####
-
+    decorate_memberships_with_tracking_data(guardian, channels, memberships)
     channels = channels.to_a
     preload_custom_fields_for(channels)
     channels
@@ -157,18 +130,37 @@ module DiscourseChat::ChatChannelFetcher
         UserField.all.pluck(:id).map { |fid| "#{User::USER_FIELD_PREFIX}#{fid}" }
     User.preload_custom_fields(channels.map { |c| c.chatable.users }.flatten, preload_fields)
 
-    unread_counts_per_channel = unread_counts(channels, user_id)
+    decorate_memberships_with_tracking_data(guardian, channels, memberships)
+  end
 
-    ##
-    # TODO (martin)
-    # Find a nicer way to do this.
+  def self.decorate_memberships_with_tracking_data(guardian, channels, memberships)
+    unread_counts_per_channel = unread_counts(channels, guardian.user.id)
+
+    mention_notifications =
+      Notification.unread.where(
+        user_id: guardian.user.id,
+        notification_type: Notification.types[:chat_mention],
+      )
+    mention_notification_data = mention_notifications.map { |m| JSON.parse(m.data) }
+
     channels.each do |channel|
       membership = memberships.find { |m| m.chat_channel_id == channel.id }
 
-      # direct message channels cannot be muted, so we always need the unread count
-      membership.unread_count = unread_counts_per_channel[channel.id]
+      if membership
+        membership.unread_mentions =
+          mention_notification_data.count do |data|
+            data["chat_channel_id"] == channel.id &&
+              data["chat_message_id"] > (membership.last_read_message_id || 0)
+          end
+
+        # direct message channels cannot be muted, so we always need the unread count
+        if channel.direct_message_channel?
+          membership.unread_count = unread_counts_per_channel[channel.id]
+        else
+          membership.unread_count = unread_counts_per_channel[channel.id] if !membership.muted
+        end
+      end
     end
-    #####
   end
 
   def self.unread_counts(channels, user_id)

discourse/discourse-chat

commit 683e491993b33ba27f54c113e67edfb3570a3a30

DEV: Asyncify chat-selection-manager (#1146)

assets/javascripts/discourse/components/chat-selection-manager.js
@@ -53,11 +53,11 @@ export default class AdminCustomizeColorsShowController extends Component {
       });
     };
 
-    if (!this.site.isMobileDevice) {
-      return this._copyQuoteToClipboard(quoteGenerationPromise);
+    if (this.site.isMobileDevice) {
+      await this._copyQuoteToComposer(quoteGenerationPromise);
+    } else {
+      await this._copyQuoteToClipboard(quoteGenerationPromise);
     }
-
-    return this._copyQuoteToComposer(quoteGenerationPromise);
   }
 
   _showCopyQuoteSuccess() {
@@ -79,33 +79,29 @@ export default class AdminCustomizeColorsShowController extends Component {
     });
   }
 
-  _goToChatableUrl() {
-    if (this.chatChannel.chatable_url) {
-      return this.router.transitionTo(this.chatChannel.chatable_url);
-    }
-  }
-
-  _copyQuoteToClipboard(quoteGenerationPromise) {
-    if (!isTesting()) {
-      return clipboardCopyAsync(quoteGenerationPromise)
-        .then(() => {
-          this._showCopyQuoteSuccess();
-        })
-        .catch(popupAjaxError);
-    } else {
+  async _copyQuoteToClipboard(quoteGenerationPromise) {
+    if (isTesting()) {
       // clipboard API throws errors in tests
       return;
+    }
+
+    try {
+      await clipboardCopyAsync(quoteGenerationPromise);
+    } catch (error) {
+      popupAjaxError(error);
     }
   }
 
   async _copyQuoteToComposer(quoteGenerationPromise) {
-    let quoteMarkdownBlob, quoteMarkdown;
+    let quoteMarkdown;
+
     try {
-      quoteMarkdownBlob = await quoteGenerationPromise();
+      const quoteMarkdownBlob = await quoteGenerationPromise();
       quoteMarkdown = await quoteMarkdownBlob.text();
     } catch (error) {
       popupAjaxError(error);
     }
+
     const container = getOwner(this);
     const composer = container.lookup("controller:composer");
     const openOpts = {};
@@ -117,18 +113,20 @@ export default class AdminCustomizeColorsShowController extends Component {
     if (this.site.isMobileDevice) {
       // go to the relevant chatable (e.g. category) and open the
       // composer to insert text
-      return this._goToChatableUrl().then(() => {
-        composer.focusComposer({
-          fallbackToNewTopic: true,
-          insertText: quoteMarkdown,
-          openOpts,
-        });
+      if (this.chatChannel.chatable_url) {
+        this.router.transitionTo(this.chatChannel.chatable_url);
+      }
+
+      await composer.focusComposer({
+        fallbackToNewTopic: true,
+        insertText: quoteMarkdown,
+        openOpts,
       });
     } else {
       // open the composer and insert text, reply to the current
       // topic if there is one, use the active draft if there is one
       const topic = container.lookup("controller:topic");
-      composer.focusComposer({
+      await composer.focusComposer({
         fallbackToNewTopic: true,
         topic: topic?.model,
         insertText: quoteMarkdown,

commit 720e1d76ee60bd2bb5b98102470f5430d862cd93

FIX: Restore and fix "quote copied" message
1. Restored the message (regressed in #1146)
2. Fixed the animation
3. Fixed element placement
4. Restored and updated tests

assets/javascripts/discourse/components/chat-selection-manager.js
@@ -64,29 +64,25 @@ export default class AdminCustomizeColorsShowController extends Component {
     this.set("showChatQuoteSuccess", true);
 
     schedule("afterRender", () => {
-      if (!this.element || this.isDestroying || this.isDestroyed) {
-        return;
-      }
-
       const element = document.querySelector(".chat-selection-message");
-      element.addEventListener(
-        "animationend",
-        () => {
-          this.set("showChatQuoteSuccess", false);
-        },
-        { once: true }
-      );
+      element?.addEventListener("animationend", () => {
+        if (this.isDestroying || this.isDestroyed) {
+          return;
+        }
+
+        this.set("showChatQuoteSuccess", false);
+      });
     });
   }
 
   async _copyQuoteToClipboard(quoteGenerationPromise) {
-    if (isTesting()) {
-      // clipboard API throws errors in tests
-      return;
-    }
-
     try {
-      await clipboardCopyAsync(quoteGenerationPromise);
+      if (!isTesting()) {
+        // clipboard API throws errors in tests
+        await clipboardCopyAsync(quoteGenerationPromise);
+      }
+
+      this._showCopyQuoteSuccess();
     } catch (error) {
       popupAjaxError(error);
     }

assets/javascripts/discourse/templates/components/chat-selection-manager.hbs
@@ -32,7 +32,7 @@
   </div>
 
   {{#if showChatQuoteSuccess}}
-    <div class="chat-selection-message alert alert-success">
+    <div class="chat-selection-message">
       {{i18n "chat.quote.copy_success"}}
     </div>
   {{/if}}

assets/stylesheets/common/chat-selection-manager.scss
@@ -1,49 +1,39 @@
 .chat-selection-management {
+  border-top: 1px solid var(--primary-low);
+  display: flex;
+  gap: 0.5rem;
+  padding: 0.5rem;
+
+  .topic-chat-drawer-content & {
+    flex-direction: column;
+  }
+
+  .chat-selection-management-buttons {
+    display: flex;
+    gap: 0.5rem;
+
+    .topic-chat-drawer-content & {
+      flex-direction: column;
+      width: 100%;
+    }
+  }
+
+  .chat-selection-message {
+    animation: chat-quote-message-background-fade-highlight 2s ease-out 3s;
+    animation-fill-mode: forwards;
+    background-color: var(--success-low);
+    color: var(--primary);
+    flex: 1;
+    line-height: normal;
+    padding: 0.5rem 0.65rem;
+  }
+
   @keyframes chat-quote-message-background-fade-highlight {
     0% {
-      background-color: var(--success-low);
-      color: black;
     }
     100% {
       background-color: transparent;
       color: transparent;
-    }
-  }
-
-  padding: 0.5em;
-  border-top: 1px solid var(--primary-low);
-  display: flex;
-
-  .chat-selection-message {
-    padding: 0.5em 0.65em;
-    margin: 0 1em;
-    flex: 1;
-    line-height: normal;
-
-    animation: chat-quote-message-background-fade-highlight 2.5s ease-out 3s;
-  }
-
-  .chat-selection-management-buttons {
-    .cancel-btn {
-      margin-left: 1em;
-    }
-  }
-}
-
-.topic-chat-drawer-content {
-  .chat-selection-management {
-    .chat-selection-management-buttons {
-      display: flex;
-      flex-direction: column;
-      width: 100%;
-
-      .cancel-btn {
-        margin-left: initial;
-      }
-
-      .btn {
-        margin-bottom: 0.25em;
-      }
     }
   }
 }

config/locales/client.en.yml
@@ -268,7 +268,7 @@ en:
 
       quote:
         original_channel: 'Originally sent in <a href="%{channelLink}">%{channel}</a>'
-        copy_success: "Chat quote copied to clipboard."
+        copy_success: "Chat quote copied to clipboard"
 
       notification_levels:
         never: "Never"

test/javascripts/acceptance/chat-quoting-test.js
@@ -1,4 +1,4 @@
-import { skip, test } from "qunit";
+import { test } from "qunit";
 import {
   click,
   currentURL,
@@ -20,8 +20,8 @@ import {
 } from "discourse/plugins/discourse-chat/chat-fixtures";
 import selectKit from "discourse/tests/helpers/select-kit-helper";
 
-let quoteResponse = {
-  markdown: `[chat quote="martinchat;3875498;2022-02-04T01:12:15Z" channel="The Beam Discussions" channelId="1234"]
+const quoteResponse = {
+  markdown: `[chat quote="martin-chat;3875498;2022-02-04T01:12:15Z" channel="The Beam Discussions" channelId="1234"]
   an extremely insightful response :)
   [/chat]`,
 };
@@ -56,16 +56,17 @@ acceptance("Discourse Chat | quoting out of topic", function (needs) {
     setupPretenders(server, helper);
   });
 
-  skip("it opens the composer and appends the quote", async function (assert) {
+  test("it copies the quote and shows a message", async function (assert) {
     await visit("/chat/channel/7/Bug");
     assert.ok(exists(".chat-message-container"));
+
     const firstMessage = query(".chat-message-container");
     await triggerEvent(firstMessage, "mouseenter");
     const dropdown = selectKit(".chat-message-container .more-buttons");
     await dropdown.expand();
     await dropdown.selectRowByValue("selectMessage");
-
     assert.ok(firstMessage.classList.contains("selecting-messages"));
+
     const quoteBtn = query(".chat-live-pane #chat-quote-btn");
     assert.equal(
       quoteBtn.disabled,
@@ -81,18 +82,8 @@ acceptance("Discourse Chat | quoting out of topic", function (needs) {
     );
 
     await click(firstMessage.querySelector("input[type='checkbox']"));
-
     await click("#chat-quote-btn");
-    assert.ok(exists("#reply-control.composer-action-createTopic"));
-    assert.strictEqual(
-      query("textarea.d-editor-input").value,
-      quoteResponse.markdown
-    );
-    assert.strictEqual(
-      selectKit(".category-chooser").header().value(),
-      "1",
-      "it fills category selector with the right category"
-    );
+    assert.ok(exists(".chat-selection-message"), "shows the message");
   });
 });
 
@@ -114,17 +105,20 @@ acceptance("Discourse Chat | quoting when topic open", async function (needs) {
     setupPretenders(server, helper);
   });
 
-  skip("it opens the composer for the topic and pastes in the quote", async function (assert) {
+  test("it opens the composer for the topic and pastes in the quote", async function (assert) {
     await visit("/t/internationalization-localization/280");
+
     await click(".header-dropdown-toggle.open-chat");
     assert.ok(visible(".topic-chat-float-container"), "chat float is open");
     assert.ok(exists(".chat-message-container"));
+
     const firstMessage = query(".chat-message-container");
     await triggerEvent(firstMessage, "mouseenter");
     const dropdown = selectKit(".chat-message-container .more-buttons");
     await dropdown.expand();
     await dropdown.selectRowByValue("selectMessage");
     assert.ok(firstMessage.classList.contains("selecting-messages"));
+
     await click("#chat-quote-btn");
     assert.ok(exists("#reply-control.composer-action-reply"));
     assert.strictEqual(
@@ -160,11 +154,12 @@ acceptance("Discourse Chat | quoting on mobile", async function (needs) {
   test("it opens the chatable, opens the composer, and pastes the markdown in", async function (assert) {
     await visit("/chat/channel/7/Bug");
     assert.ok(exists(".chat-message-container"));
+
     const firstMessage = query(".chat-message-container");
     await tap(firstMessage);
     await click(".chat-message-action-item[data-id='selectMessage'] button");
-
     assert.ok(firstMessage.classList.contains("selecting-messages"));
+
     await click("#chat-quote-btn");
     await settled();
 
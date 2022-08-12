43a69a58fc7b741db23dbccf54aedcdf4ede85de
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-08-12 20:03:50 +0400

    FIX: Don't show user status on direct message channels with multiple users (#1164)

--- test/javascripts/components/chat-channel-row-test.js
+++ test/javascripts/components/chat-channel-row-test.js
@@ -127,4 +127,28 @@ module("Discourse Chat | Component | chat-channel-row", function (hooks) {
       assert.ok(exists(".emoji[title='Off to dentist']"));
     },
   });
+
+  componentTest(
+    "doesn't show user status on a direct message channel with multiple users",
+    {
+      template: hbs`{{chat-channel-row channel=channel}}`,
+
+      beforeEach() {
+        const status = { description: "Off to dentist", emoji: "tooth" };
+        const channel = fabricators.directMessageChatChannel();
+        channel.chatable.users[0].status = status;
+        channel.chatable.users.push({
+          id: 2,
+          username: "bill",
+          name: null,
+          avatar_template: "/letter_avatar_proxy/v3/letter/t/31188e/{size}.png",
+        });
+        this.set("channel", channel);
+      },
+
+      async test(assert) {
+        assert.notOk(exists(".emoji[title='Off to dentist']"));
+      },
+    }
+  );
 });

0ae291158d78e9fdbe7bd40f35d8dd1e1a1431a7
Roman Rizzi <rizziromanalejandro@gmail.com>
2022-08-23 15:04:18 -0300

    FIX: Display error when switching channels returns a 429 (#1182)

--- test/javascripts/acceptance/chat-live-pane-test.js
+++ test/javascripts/acceptance/chat-live-pane-test.js
@@ -170,3 +170,56 @@ acceptance(
     });
   }
 );
+
+acceptance(
+  "Discourse Chat - Chat live pane - handling 429 errors",
+  function (needs) {
+    needs.user({
+      username: "eviltrout",
+      id: 1,
+      has_chat_enabled: true,
+    });
+    needs.settings({
+      chat_enabled: true,
+    });
+
+    needs.pretender((server, helper) => {
+      server.get("/chat/:chatChannelId/messages.json", () => {
+        return helper.response(429);
+      });
+
+      server.get("/chat/chat_channels.json", () =>
+        helper.response({
+          public_channels: [
+            {
+              id: 1,
+              title: "something",
+              current_user_membership: { following: true },
+            },
+          ],
+          direct_message_channels: [],
+        })
+      );
+
+      server.get("/chat/chat_channels/:chatChannelId", () =>
+        helper.response({ id: 1, title: "something" })
+      );
+
+      server.post("/chat/drafts", () => {
+        return helper.response([]);
+      });
+
+      server.post("/chat/:chatChannelId.json", () => {
+        return helper.response({ success: "OK" });
+      });
+    });
+
+    test("Handles 429 errors by displaying an alert", async function (assert) {
+      await visit("/chat/channel/1/cat");
+
+      assert.ok(exists(`.bootbox`), "We displayed a 429 error");
+
+      await click(".bootbox span.d-button-label");
+    });
+  }
+);

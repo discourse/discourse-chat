7eb312c469a910f8c285e424f627bc91bc5a0053
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-08-30 21:56:48 +0200

    better test

--- test/javascripts/acceptance/chat-composer-test.js
+++ test/javascripts/acceptance/chat-composer-test.js
@@ -77,7 +77,7 @@ acceptance("Discourse Chat - Composer - unreliable network", function (needs) {
   test("Draft with unreliable network", async function (assert) {
     this.chatService.set("chatWindowFullPage", false);
     await visit("/chat/channel/11/-");
-    await fillIn(".chat-composer-input", "network-error-message");
+    this.chatService.set("isNetworkUnreliable", true);
     await settled();
 
     assert.ok(

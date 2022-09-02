138ce9bb8d5fef18fc38ec23ae4f279739a4f93a
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2022-09-03 01:47:41 +1000

    FIX: Move types of the chat notification list to the tab class (#1225)
    This change is needed following this commit in core: https://github.com/discourse/discourse/commit/661a903a0b3a8e7d19a9dad4fbeddefb49548b5b
    
    Co-authored-by: OsamaSayegh <asooomaasoooma90@gmail.com>

--- assets/javascripts/discourse/components/user-menu/chat-notifications-list.js
+++ assets/javascripts/discourse/components/user-menu/chat-notifications-list.js
@@ -1,7 +1,7 @@
 import UserMenuNotificationsList from "discourse/components/user-menu/notifications-list";
 
 export default class UserMenuChatNotificationsList extends UserMenuNotificationsList {
-  get filterByTypes() {
-    return ["chat_mention", "chat_message", "chat_invitation", "chat_quoted"];
+  get dismissTypes() {
+    return this.filterByTypes;
   }
 }

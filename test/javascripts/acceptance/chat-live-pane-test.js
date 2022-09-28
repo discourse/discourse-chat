788f4650ce4fbb14aa147a3ee0c50045d5b27b6b
Penar Musaraj <pmusaraj@gmail.com>
2022-09-28 15:25:34 -0400

    Revert "DEV: Skip a test temporarily (#1257)"
    This reverts commit d0cc1bc6c61cf6dbd4bc9a0765edc40337854f63.

--- test/javascripts/acceptance/chat-live-pane-test.js
+++ test/javascripts/acceptance/chat-live-pane-test.js
@@ -5,7 +5,7 @@ import {
   publishToMessageBus,
   query,
 } from "discourse/tests/helpers/qunit-helpers";
-import { skip, test } from "qunit";
+import { test } from "qunit";
 
 function buildMessage(messageId) {
   return {
@@ -214,7 +214,7 @@ acceptance(
       });
     });
 
-    skip("Handles 429 errors by displaying an alert", async function (assert) {
+    test("Handles 429 errors by displaying an alert", async function (assert) {
       await visit("/chat/channel/1/cat");
 
       assert.ok(exists(`.bootbox`), "We displayed a 429 error");

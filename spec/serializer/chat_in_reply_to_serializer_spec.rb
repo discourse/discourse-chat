c2a3f30d1dea86b550a5136fac7461a8f490ed64
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-10-04 16:01:33 +0200

    refactor in_reply_to spec

--- spec/serializer/chat_in_reply_to_serializer_spec.rb
+++ spec/serializer/chat_in_reply_to_serializer_spec.rb
@@ -2,30 +2,32 @@
 
 require "rails_helper"
 
-describe ChatInReplyToSerializer do
+RSpec.describe ChatInReplyToSerializer do
+  subject(:serializer) { described_class.new(message, scope: guardian, root: nil) }
+
   fab!(:chat_channel) { Fabricate(:chat_channel) }
-  fab!(:message_1) { Fabricate(:chat_message, user: Fabricate(:user), chat_channel: chat_channel) }
   let(:guardian) { Guardian.new(Fabricate(:user)) }
 
-  subject { described_class.new(message_1, scope: guardian, root: nil) }
-
   describe "#user" do
-    context "when user has been destroyed" do
-      it "returns a placeholder user" do
-        message_1.user.destroy!
-        message_1.reload
+    let(:message) { Fabricate(:chat_message, user: Fabricate(:user), chat_channel: chat_channel) }
 
-        expect(subject.as_json[:user][:username]).to eq(I18n.t("chat.deleted_chat_username"))
+    context "when user has been destroyed" do
+      before do
+        message.user.destroy!
+        message.reload
+      end
+
+      it "returns a placeholder user" do
+        expect(serializer.as_json[:user][:username]).to eq(I18n.t("chat.deleted_chat_username"))
       end
     end
   end
 
   describe "#excerpt" do
-    it "censors words" do
-      watched_word = Fabricate(:watched_word, action: WatchedWord.actions[:censor])
-      message = Fabricate(:chat_message, message: "ok #{watched_word.word}")
-      serializer = described_class.new(message, scope: guardian, root: nil)
+    let(:watched_word) { Fabricate(:watched_word, action: WatchedWord.actions[:censor]) }
+    let(:message) { Fabricate(:chat_message, message: "ok #{watched_word.word}") }
 
+    it "censors words" do
       expect(serializer.as_json[:excerpt]).to eq("ok ■■■■■")
     end
   end

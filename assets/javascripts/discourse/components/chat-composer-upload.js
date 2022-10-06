c6bbdc0b9ae8780009a724f8d6ee11b4cda7fe93
Martin Brennan <martin@discourse.org>
2022-10-06 16:18:41 +1000

    FIX: Replace use of IMAGES_EXTENSIONS_REGEX in chat-composer-upload

--- assets/javascripts/discourse/components/chat-composer-upload.js
+++ assets/javascripts/discourse/components/chat-composer-upload.js
@@ -1,6 +1,6 @@
 import Component from "@ember/component";
 import discourseComputed from "discourse-common/utils/decorators";
-import { IMAGES_EXTENSIONS_REGEX } from "discourse/lib/uploads";
+import { isImage } from "discourse/lib/uploads";
 
 export default Component.extend({
   IMAGE_TYPE: "image",
@@ -11,9 +11,9 @@ export default Component.extend({
   upload: null,
   onCancel: null,
 
-  @discourseComputed("upload.extension")
-  type(extension) {
-    if (IMAGES_EXTENSIONS_REGEX.test(extension)) {
+  @discourseComputed("upload.original_filename")
+  type(filename) {
+    if (isImage(filename)) {
       return this.IMAGE_TYPE;
     }
   },

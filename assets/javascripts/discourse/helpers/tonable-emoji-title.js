e2c5491bb62e94004c9ffc0d611724681117656d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2022-10-19 18:09:39 +0200

    better name

--- assets/javascripts/discourse/helpers/tonable-emoji-title.js
+++ assets/javascripts/discourse/helpers/tonable-emoji-title.js
@@ -1,9 +1,9 @@
 import { registerUnbound } from "discourse-common/lib/helpers";
 
-registerUnbound("tonable-emoji-title", function (emoji, scale) {
-  if (!emoji.tonable || scale === 1) {
+registerUnbound("tonable-emoji-title", function (emoji, diversity) {
+  if (!emoji.tonable || diversity === 1) {
     return `:${emoji.name}:`;
   }
 
-  return `:${emoji.name}:t${scale}:`;
+  return `:${emoji.name}:t${diversity}:`;
 });
